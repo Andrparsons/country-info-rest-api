29f1237ef720a0545764743c242a48bcf7726b3e
andrparsons <andrparsons@gmail.com>
2019-06-17 14:15:20 -0400

    installed scss and set colours

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Where in the world?`,
+    description: `A Frontend Mentor code challenge. Integrate with the REST Countries API to pull data and display as per design docs`,
+    author: `Andre Parsons`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
